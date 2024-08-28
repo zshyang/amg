da9b9f3ddbb8a00a1e18bfedd0208c4bead5baa1
zshyang <zshyang1106@gmail.com>
2024-08-27 18:56:59 -0700

    add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='amg',
+    version='0.0.1',
+    description='zhangsihao yang amg for python',
+    packages=find_packages(),
+    install_requires=[],
+)
